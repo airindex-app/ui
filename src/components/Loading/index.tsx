eb992462edbeec2d701bb755a1088ed2792a4afa
Jackson Dolman <mail@dolmios.com>
2025-07-21 16:36:50 -0400

    refactor: ensure loading component height matches width when overriding

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -4,5 +4,12 @@ import { type ILoading } from "../../index";
 import { LoadingStyled } from "./styles";
 
 export default function Loading({ css, width }: ILoading): JSX.Element {
-  return <LoadingStyled css={{ width, ...css }} />;
+  return (
+    <LoadingStyled
+      css={{
+        ...(width ? { height: width, width } : {}),
+        ...css,
+      }}
+    />
+  );
 }
